a34e869c8ad8322c2ea008108fea0adae40fc07a
Jarrod Taylor <email@jarrodtaylor.com>
2024-01-24 14:57:34 -0500

    Fills out URL

--- Sources/Foundation/URL.swift
+++ Sources/Foundation/URL.swift
@@ -5,6 +5,19 @@ extension URL {
     var isDirectory: ObjCBool = true
     return FileManager.default.fileExists(atPath: self.path(), isDirectory: &isDirectory)
   }
+  
+  var files: [URL] { list.filter { !$0.isDirectory } }
+  
+  var folders: [URL] { list.filter { $0.isDirectory } }
+  
+  var isDirectory: Bool { (try? resourceValues(forKeys: [.isDirectoryKey]))?.isDirectory == true }
+  
+  var list: [URL] {
+    FileManager.default
+      .subpaths(atPath: self.path())!
+      .filter { !$0.contains(".DS_Store") }
+      .map { self.appending(component: $0) }
+  }
 
   var masked: String {
     absoluteString
